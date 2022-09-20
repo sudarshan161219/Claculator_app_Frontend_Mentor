1190d2bde68f9c5ce0bc2cd693d91f348fc63eda
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-09-20 18:36:26 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
 const three = document.querySelector(".three");
-
-
 const body = document.body
 
 one.addEventListener("click", ()=> {
@@ -24,3 +22,96 @@ three.addEventListener("click", ()=> {
   body.classList.add("body-three");
     }
 });
+
+// const previousOperandText = document.querySelector('[data-previous-operand]');
+// const currentOperandText = document.querySelector('.current-operand');
+
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+const buttons = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const resetButton = document.querySelector('[data-all-clear]');
+const equalButton = document.querySelector('[data-equals]');
+
+// const calculator = new Calculator(previousOperandText, currentOperandText)
+
+
+
+
+
+buttons.forEach(button => {
+button.addEventListener("click", (e)=>{
+ currentOperandText.innerText += e.target.innerText 
+
+});
+});
+
+operationButton.forEach(opb => {
+  opb.addEventListener("click", (e)=>{
+
+  });
+  });
+
+  deleteButton.addEventListener("click", () => {
+
+
+  });
+
+  resetButton.addEventListener("click", () => {
+calculator.clear()
+
+
+  });
+
+
+   equalButton.addEventListener("click", () => {
+console.log (compute(1, 2))
+
+  });
+
+
+
+
+  function appendNumber () {
+
+    // currentOperandText.innerText = 
+
+  }
+
+  function operation () {
+
+  }
+
+  function compute(){
+    let computation
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if(isNaN(prev) || isNaN(current)){
+        return
+    }
+    
+    switch(this.operation){
+        case '+':
+            computation = prev + current
+            break
+            case '-':
+                computation = prev - current
+                break
+                case '*':
+                    computation = prev * current
+                    break
+                    case '÷':
+                        computation = prev / current
+                        break
+    
+                        default:
+                            return
+    
+    }
+    
+    // this.currentOperand = computation
+    // this.operation = undefined
+    // this.previousOperand = ''
+    
+    }
