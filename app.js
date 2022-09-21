e1520eef2af4678ddbc765a07abefb0790859892
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-09-21 19:41:58 +0530

    update

--- app.js
+++ app.js
@@ -24,108 +24,103 @@ three.addEventListener("click", () => {
 });
 
 class Calculator {
+constructor(previousOperandText, currentOperandText){
+    this.previousOperandText = previousOperandText
+    this.currentOperandText = currentOperandText
+    this.clear()
+}
+   
+clear(){
+    this.previousOperand = ''
+    this.currentOperand = ''
+    this.operation = undefined
+}
 
-    constructor(previousOperandText, currentOperandText){
-        this.previousOperandText = previousOperandText
-        this.currentOperandText = currentOperandText
-        this.clear()
+delete(){
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+}
+
+appendNumber(number){
+    if(number === '.' && this.currentOperand.includes('.')){
+        return
     }
-
-    clear(){
-        this.previousOperand = ''
-        this.currentOperand = ''
-        this.operation = undefined
-    }
-
-    delete(){
-        this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
-
-
-    appendNumber(number){
-        if(number === '.' && this.currentOperand.includes('.')){
-            return
-        }
-        this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
-
-
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
 
 chooseOperation(operation){
     if(this.currentOperand === ''){
         return
-    } 
-
-    if(this.previousOperand != ''){
+    }
+    if(this.previousOperand !== ''){
         this.compute()
     }
-this.operation = operation
-this.previousOperand = this.currentOperand
-this.currentOperand = ''
+
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
 }
 
 compute(){
-let computation
-let prev = parseFloat(this.previousOperand) 
-let current = parseFloat(this.currentOperand)
-if(isNaN(prev) || isNaN(current)){
-    return
-}
-
-switch (this.operation) {
-    case '+':
-        computation = prev + current
-        break;
-           case '-':
-        computation = prev - current
-        break;
-           case '*':
-        computation = prev * current
-        break;
-           case '÷':
-        computation = prev / current
-        break;
-
-    default:
+    let computation
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if(isNaN(prev) || isNaN(current)){
         return
-        
-}
+    }
 
-this.currentOperand = computation
-this.previousOperand = ''
-this.operation = undefined
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break;
 
-}
+            case '-':
+                computation = prev - current
+                break;
 
+                case 'x':
+                    computation = prev * current
+                    break;
+
+                    case '/':
+                        computation = prev / current
+                        break;
+                          default:
+                              return
+    }
+
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+} 
 
 getDisplayNumber(number){
-const stringNumber = number.toString();
-const interDigits = parseFloat(stringNumber.split('.')[0]);
-const decimalDigits = stringNumber.split('.')[1];
-let interDisplay
-if(isNaN(interDigits)){
-    interDisplay = ''
-}else{
-    interDisplay = interDigits.toLocaleString('en', {maximumFractionDigits:0})
-}
-
-if(decimalDigits != null){
-    return `${interDisplay}.${decimalDigits}`
-}else{
-    return interDisplay
-}
-
-}
-
-    updateDisplay(){
-        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand) 
-if(this.operation != null){
-this.previousOperandText.innerText =
-`${this.getDisplayNumber(this.previousOperand)}  ${this.operation}`
-}else{
-    this.previousOperandText.innerText = ''
-}
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay 
+    if(isNaN(integerDigits)){
+        integerDisplay = ''
+    }else{
+        integerDisplay  = integerDigits.toLocaleString('en', {maximumFractionDigits:0})
     }
+    if(decimalDigits != null){
+      return  `${integerDisplay}.${decimalDigits}`
+    }else{
+     return   integerDisplay
+    }
+
+}
+
+updateDisplay(){
+    this.currentOperandText.innerText = this.getDisplayNumber( this.currentOperand)
+    if(this.operation != null){
+            this.previousOperandText.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }else{
+        this.previousOperandText.innerText = ''
+    }
+
+}
 
 }
 
@@ -176,3 +171,31 @@ calculator.updateDisplay();
 
 
 
+// getDisplayNumber(number){
+// const stringNumber = number.toString();
+// const interDigits = parseFloat(stringNumber.split('.')[0]);
+// const decimalDigits = stringNumber.split('.')[1];
+// let interDisplay
+// if(isNaN(interDigits)){
+// interDisplay = ''
+// }else{
+// interDisplay = interDigits.toLocaleString('en', {maximumFractionDigits:0})
+// }
+
+// if(decimalDigits != null){
+// return `${interDisplay}.${decimalDigits}`
+// }else{
+// return interDisplay
+// }
+
+// }
+
+// updateDisplay(){
+//     this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand) 
+// if(this.operation != null){
+// this.previousOperandText.innerText =
+// `${this.getDisplayNumber(this.previousOperand)}  ${this.operation}`
+// }else{
+// this.previousOperandText.innerText = ''
+// }
+// }
